aee80dbce1ef9c9f0ab32ac321592db8f1ac27f2
Suyash Kumar <suyashkumar2003@gmail.com>
2020-02-01 14:28:28 -0800

    Add display helper

--- firmware/src/display.h
+++ firmware/src/display.h
@@ -1,8 +1,17 @@
-//
-// Created by Suyash Kumar on 2/1/20.
-//
-
 #ifndef FIRMWARE_DISPLAY_H
 #define FIRMWARE_DISPLAY_H
+#include <Adafruit_SSD1306.h>
+
+class BikeDisplay {
+public:
+    BikeDisplay();
+
+    void Init();
+    void UpdateRevs(const int& revs);
+
+private:
+    void PrepareDisplayForPrint(bool is_center_cursor=false);
+    Adafruit_SSD1306 display;
+};
 
 #endif //FIRMWARE_DISPLAY_H
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,46 +1,26 @@
 /* 
- * Initial WIP program to sample an excercise bike's 
+ * Initial WIP program to sample an exercise bike's
  * magnetic reed switch. 
  * @author Suyash Kumar <suyash@suyashkumar.com>
  */
 #include "Arduino.h"
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 13
-#endif 
+#include "display.h"
 
 #define THRESHOLD 300 
 #define VERIFY_ITER_N 10
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
 bool previous_trigger_state = false;
 int num_revolutions = 0;
+BikeDisplay display;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(A0, INPUT);
   Serial.begin(9600);
-  Serial.println("Start.");
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-  display.clearDisplay();
-  display.setTextSize(2);             
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 16 - 4);      
-  display.println(F("Bike!"));
-  display.display();
+  display.Init();
   delay(2000);
-
 }
 
 bool switch_is_triggered() {
@@ -66,22 +46,12 @@ void loop() {
     // New unique revolution
     previous_trigger_state = true;
 	num_revolutions += 1;
-  	display.clearDisplay();
-  	display.setTextSize(2);             
-  	display.setTextColor(SSD1306_WHITE);
-  	display.setCursor(0, 16 - 4);      
-  	display.print(F("Revs: "));
-  	display.print(num_revolutions);
-  	display.display();
+	display.UpdateRevs(num_revolutions);
 
-    // Turn the LED on if the magnetic reed switch is currently triggered
-    digitalWrite(LED_BUILTIN, LOW);
   } else if (!switch_is_triggered() && previous_trigger_state) {
     // Switch is not triggered anymore, update previous trigger state
 	previous_trigger_state = false;
-    // Turn the LED off if the magnetic reed switch is not triggered
-    digitalWrite(LED_BUILTIN, HIGH);
-  } 
+  }
 
   delay(10);
 }

837ad95779f395d5287832206d305762d4c22427
Suyash Kumar <suyashkumar2003@gmail.com>
2020-02-01 14:28:08 -0800

    Add display helper

---
+++ firmware/src/display.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Suyash Kumar on 2/1/20.
+//
+
+#include "display.h"
---
+++ firmware/src/display.h
@@ -0,0 +1,8 @@
+//
+// Created by Suyash Kumar on 2/1/20.
+//
+
+#ifndef FIRMWARE_DISPLAY_H
+#define FIRMWARE_DISPLAY_H
+
+#endif //FIRMWARE_DISPLAY_H
